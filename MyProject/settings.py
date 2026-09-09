d5a4f2c844527439672c8047ac0604876a49bcf5
Mayank Rajpoot <krishnaraj141106@gmail.com>
2026-09-09 13:29:34 +0530

    Added WhiteNoise

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +126,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_DIR = BASE_DIR/"static"
 STATICFILES_DIRS=[STATIC_DIR]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
